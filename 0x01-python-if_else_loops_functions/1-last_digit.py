da97b3366e6e232742df227fc394495447f92235
arleybri18 <arleybri18@gmail.com>
2019-04-30 20:02:32 +0000

    quit function abs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, -(abs(number) % 10)))
+    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, -(-number % 10)))
 else:
     if (number % 10) > 5:
         print("Last digit of {0} is {1} and is greater than 5".format(number, (number % 10)))

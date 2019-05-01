31129fa0f9cd2afc1c54e541e2bc95f506f2b69e
arleybri18 <arleybri18@gmail.com>
2019-05-01 03:32:35 +0000

    modify order in the if

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if (ord(c) > 96 and ord(c) < 123):
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{}".format(c), end="")
-    print("\n")
+        print("{:c}".format((ord(c) - 32)
+              if (ord(c) > 96 and ord(c) < 123)
+              else ord(c)), end="")
+    print("")

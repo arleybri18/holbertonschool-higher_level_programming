5f5f3097a1515e40b3499ea873c4f53436bc0358
arleybri18 <arleybri18@gmail.com>
2019-05-01 03:08:49 +0000

    condition for negative index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    string = str[:n] + str[n + 1:]
+    if n >=0:
+        string = str[:n] + str[n + 1:]
+    else:
+        string = str
     return (string)

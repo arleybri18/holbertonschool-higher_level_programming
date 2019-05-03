cc55fe5b2a85606c38de4e82eeab0ed95a5ab0ac
arleybri18 <arleybri18@gmail.com>
2019-05-03 03:05:04 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    sum =  0
+    argument = len(sys.argv)
+    if argument == 1:
+        print(0)
+    elif argument >= 2:
+        for i in range(1, argument):
+            sum = sum + int(sys.argv[i])
+        print("{:d}".format(sum))
+

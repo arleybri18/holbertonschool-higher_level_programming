2fe6d24cd7fb6ea59453d9696a4ffb9f41d8b93a
arleybri18 <arleybri18@gmail.com>
2019-05-07 17:57:55 +0000

    add condition >= in the comparison

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list)):
+    if idx < 0 or idx >= (len(my_list)):
         return my_list
     else:
         new_list = my_list[:]

35c3acef04456b7196dbd70a52fb28d923ec9f84
arleybri18 <arleybri18@gmail.com>
2019-05-07 19:48:03 +0000

    delete at index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if len(my_list) == 0 and idx > len(my_list):
+        return my_list
+    else:
+        return (my_list[0 : idx] + my_list[idx + 1 :])

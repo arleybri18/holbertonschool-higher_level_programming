33598a5e20337b463df8630ef99b412d47821401
arleybri18 <arleybri18@gmail.com>
2019-05-07 20:42:05 +0000

    iterate and return new_string

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new_list = []
         for index in range(len(my_list)):
-            if index != idx:
-                new_list.append(my_list[index])
-        return new_list
+            if index == idx:
+                del my_list[index]
+        return my_list

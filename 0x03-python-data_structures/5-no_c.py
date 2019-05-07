660428156ff3920bb4cdd8833c2db77a3bb2d7e9
arleybri18 <arleybri18@gmail.com>
2019-05-07 18:06:36 +0000

    empty string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/bin/usr/python3
 def no_c(my_string):
-    new_str = ""
+    new_str = ''
     for char in my_string:
         if char != 'c' and char != 'C':
             new_str = new_str + char

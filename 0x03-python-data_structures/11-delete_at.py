8382db5867f153f2cfb0e377c90ee147c813e6f2
arleybri18 <arleybri18@gmail.com>
2019-05-07 19:55:42 +0000

    adjust for pep8

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,4 +3,4 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        return (my_list[0 : idx] + my_list[idx + 1 :])
+        return (my_list[0:idx] + my_list[idx + 1:])

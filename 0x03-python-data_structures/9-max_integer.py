210e61fa0650df72b951aa9e2f0129e439d8ae41
arleybri18 <arleybri18@gmail.com>
2019-05-07 19:16:00 +0000

    return max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        my_list.sort(reverse = True)
+        my_list.sort(reverse=True)
         return my_list[0]

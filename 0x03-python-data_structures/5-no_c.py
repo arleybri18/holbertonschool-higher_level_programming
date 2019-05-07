3674902071c6ee0a9f321c38925fe31e50e7deed
arleybri18 <arleybri18@gmail.com>
2019-05-07 20:20:03 +0000

    return string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({'c': None, 'C': None})
+    new_str = my_string.translate({ord('c'): None, ord('C'): None})
+    return new_str

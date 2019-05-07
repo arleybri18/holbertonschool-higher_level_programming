21a8c202573cf85f371780133d0470954cddc173
arleybri18 <arleybri18@gmail.com>
2019-05-07 17:42:02 +0000

    Programs using lists and tuples

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+def no_c(my_string):
+    new_str = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str = new_str + char
+    return new_str
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for nlist in matrix:
+        i = 1
+        if len(nlist) == 0:
+            print("")
+        else:
+            for el in nlist:
+                if i < len(nlist):
+                    print("{}".format(el), end=" ")
+                else:
+                    print("{}".format(el), end="\n")
+                i += 1

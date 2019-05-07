bf9449f95f125f22e10c2dfd7e718e7a8de2daa6
arleybri18 <arleybri18@gmail.com>
2019-05-07 18:28:55 +0000

    using function translate

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/bin/usr/python3
 def no_c(my_string):
-    new_str = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_str = new_str + char
+    new_str = my_string.translate({'c': None, 'C': None})
     return new_str

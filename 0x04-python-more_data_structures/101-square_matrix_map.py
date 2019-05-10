58bc711e36900423d0aa15925952c5c3e3615c42
arleybri18 <arleybri18@gmail.com>
2019-05-10 05:45:20 +0000

    rename variables

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda intern_list:
-                list(map(lambda num: num**2, intern_list)), matrix))
+    return list(map(lambda rw: list(map(lambda num: num**2, rw)), matrix))

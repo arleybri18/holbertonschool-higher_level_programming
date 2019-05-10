670ae77a5f9b9d089a72a950b9e90997e320128b
arleybri18 <arleybri18@gmail.com>
2019-05-10 05:39:38 +0000

    delete assign lambda

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x**2, row)) for row in matrix]
+    return list(map(lambda intern_list:
+                list(map(lambda num: num**2, intern_list)), matrix))

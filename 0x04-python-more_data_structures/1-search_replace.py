7d5e05d816e69430e192e138caae42bb2260fd04
arleybri18 <arleybri18@gmail.com>
2019-05-10 01:21:43 +0000

    modify remove lambda function using list comprehensive

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    rep = lambda elem, search, replace: replace if search == elem else elem
-    return [rep(elem, search, replace) for elem in my_list]
-    
+    return [replace if elem == search else elem for elem in my_list]

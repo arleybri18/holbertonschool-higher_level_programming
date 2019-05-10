9909e9177ef1e0b4f48ad850760f7de2d6b2872e
arleybri18 <arleybri18@gmail.com>
2019-05-10 03:43:53 +0000

    function that sum unique elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = list(set(my_list))
-    for el in new_list:
-        print(el)
-        el = el + el
-    return el
+    unique = set(my_list)
+    res = sum(unique)
+    return res

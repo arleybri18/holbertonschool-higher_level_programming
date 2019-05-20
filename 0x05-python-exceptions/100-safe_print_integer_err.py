521b6b3f84f25d18bf90390e29d1d0233c519cbb
arleybri18 <arleybri18@gmail.com>
2019-05-20 22:45:51 +0000

    print a message with a capture exception message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        pirint("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
+        return False

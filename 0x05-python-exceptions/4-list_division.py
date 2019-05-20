a5211bfddc0e891e110b65246c245fb0c5c8f674
arleybri18 <arleybri18@gmail.com>
2019-05-20 22:08:11 +0000

    add finally clause and reordered try except

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,15 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for idx in range(list_length):
+        res = 0
         try:
-            result.append(my_list_1[idx] / my_list_2[idx])
+            res = my_list_1[idx] / my_list_2[idx]
         except ZeroDivisionError:
-            result.append(0)
             print("division by 0")
         except IndexError:
-            result.append(0)
             print("out of range")
         except (TypeError, ValueError):
-            result.append(0)
             print("wrong type")
+        finally:
+            result.append(res)
     return result

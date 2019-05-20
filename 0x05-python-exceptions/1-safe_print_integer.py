33b2a6d4b790ba217ce08707e0b726bef4082f51
arleybri18 <arleybri18@gmail.com>
2019-05-20 20:45:03 +0000

    solver error pep8

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-# capture exception if type is not integer
     try:
         print("{:d}".format(value))
+        return True
     except ValueError:
         return False
-    return True

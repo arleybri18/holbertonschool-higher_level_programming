74a7d716e743331c3cfd3a7d073413a7e1789479
arleybri18 <arleybri18@gmail.com>
2019-05-20 20:33:41 +0000

    handle simple exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     y = 0
+# capture exception if my_list[el] is out of range
     try:
         for el in range(x):
             print(my_list[el], end="")

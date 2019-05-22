d2870601a7a570c96e7b58607ca040b18fbcbd87
arleybri18 <arleybri18@gmail.com>
2019-05-22 04:02:28 +0000

    print with spaces of the position tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,7 +69,7 @@ class Square:
         Args:
             value: value to set a position
         """
-        if (type(value) is not tuple or len(value) > 2 or type(value[0])
+        if (type(value) is not tuple or len(value) is not 2 or type(value[0])
                 is not int or type(value[1]) is not int or value[0] < 0 or
                 value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")

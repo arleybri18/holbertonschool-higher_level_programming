324a43b5274426214b34cff4bd6e8605e84df3c4
arleybri18 <arleybri18@gmail.com>
2019-06-04 19:47:47 +0000

    inheritance excersices

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
+    '''validate if an object belongs to a_class or other class inherited'''
+    return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    return issubclass(obj, a_class)

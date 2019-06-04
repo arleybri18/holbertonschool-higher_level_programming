901d3adaa308534ab4689e5c013719f389cc66ab
arleybri18 <arleybri18@gmail.com>
2019-06-04 20:50:17 +0000

    inheritance excersices

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Class Square inherit from Rectangle'''
+    def __init__(self, size):
+        '''init method'''
+        self.__size = super().integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        '''Calculate area of the rectangle
+        arg width (int): width of the rectangle
+        arg height (int): height of the rectangle
+        return area'''
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Class Rectangle inherit from BaseGeometry'''
+    def __init__(self, width, height):
+        '''init method
+        arg width (int): width of the rectangle
+        arg height (int): height of the rectangle'''
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
+    def area(self):
+        '''Calculate area of the rectangle
+        arg width (int): width of the rectangle
+        arg height (int): height of the rectangle
+        return area'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)

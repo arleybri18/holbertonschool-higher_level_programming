0aea9177592dae2e66525a150c6991514af12a0e
arleybri18 <arleybri18@gmail.com>
2019-06-05 16:29:07 +0000

    handle files

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    lines = 0
+    with open(filename, 'r') as f:
+        if nb_lines <= 0:
+            for line in f:
+                print(line, end='')
+        else:
+            for i in range(nb_lines):
+                line_data = f.readline()
+                if (line_data != ''):
+                    print(line_data, end='')

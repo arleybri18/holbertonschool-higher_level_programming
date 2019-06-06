b41364687ba2933696cbe8ebae440e58253cf2f7
arleybri18 <arleybri18@gmail.com>
2019-06-06 01:47:50 +0000

    return string to json

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end='')
--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+import json
+
+
 def from_json_string(my_str):
+    data = json.loads(my_str)
+    return data

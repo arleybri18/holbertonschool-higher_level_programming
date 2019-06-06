1bfc37ee68c274bd1be20e6e97fe45319bef218c
arleybri18 <arleybri18@gmail.com>
2019-06-06 02:51:06 +0000

    class to json

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+import os
+import json
+import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+new_list = []
+
+if 

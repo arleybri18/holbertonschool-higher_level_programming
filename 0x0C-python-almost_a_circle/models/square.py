928901199d4b066ad9fb72bba1fa1562de64f0f2
arleybri18 <arleybri18@gmail.com>
2019-06-10 17:37:18 +0000

    implement setter and getter for class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,14 @@ class Square(Rectangle):
         return "[{}] ({}) {}/{} - {}".format(
             self.__class__.__name__, self.id, self.x,
             self.y, self.width)
+    @property
+    def size(self):
+        '''getter for size'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''setter for size'''
+        self.width = value
+        self.height = value
+

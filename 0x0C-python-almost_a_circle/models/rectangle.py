649593e12f8128fbd21004d3cc351f20c0c0ebcc
arleybri18 <arleybri18@gmail.com>
2019-06-11 19:24:49 +0000

    adjust rectangle.py change is not for !=

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''setter for width'''
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif (value <= 0):
             raise ValueError("width must be > 0")
@@ -42,7 +42,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         '''setter for height'''
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif (value <= 0):
             raise ValueError("height must be > 0")
@@ -57,7 +57,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         '''setter for x'''
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         elif (value < 0):
             raise ValueError("x must be >= 0")
@@ -72,7 +72,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         '''setter for y'''
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("y must be an integer")
         elif (value < 0):
             raise ValueError("y must be >= 0")

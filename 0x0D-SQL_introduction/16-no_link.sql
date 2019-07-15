06aaab275b5edabb6098dbfc3707e10f5e6d937e
arleybri18 <arleybri18@gmail.com>
2019-07-15 19:07:50 +0000

    No list NULL values

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Lists rows with name value
 -- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name!="";

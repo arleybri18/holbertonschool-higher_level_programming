dadce7517963af14819eb55c184bc8686bf664c4
arleybri18 <arleybri18@gmail.com>
2019-07-16 23:53:37 +0000

    LEFT JOIN with WHERE clausule

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Select LEFT JOIN with Where clausule
+-- Execute: cat 14-my_genres.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT b.name
+FROM tv_show_genres a
+LEFT JOIN tv_genres b
+ON a.genre_id = b.id
+WHERE a.show_id = 8
+ORDER BY 1 ASC;

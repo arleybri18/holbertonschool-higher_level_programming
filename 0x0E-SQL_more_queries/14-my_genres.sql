5ed5631ccb636a7d2cba259a2307247ad99c9c00
arleybri18 <arleybri18@gmail.com>
2019-07-17 00:07:16 +0000

    USE two LEFT JOIN

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,10 @@
 -- Select LEFT JOIN with Where clausule
 -- Execute: cat 14-my_genres.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
-SELECT b.name
-FROM tv_show_genres a
-LEFT JOIN tv_genres b
-ON a.genre_id = b.id
-WHERE a.show_id = (SELECT id FROM tv_shows WHERE title = "Dexter")
+SELECT a.name
+FROM tv_genres a
+LEFT JOIN tv_show_genres b
+ON a.id = b.genre_id
+LEFT JOIN tv_shows c
+ON b.show_id = c.id
+WHERE c.title = 'Dexter'
 ORDER BY 1 ASC;

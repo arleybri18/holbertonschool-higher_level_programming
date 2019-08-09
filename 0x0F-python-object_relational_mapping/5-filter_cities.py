9c369772c285c4546d89b6f899aacdb637d1c0c2
arleybri18 <arleybri18@gmail.com>
2019-08-09 22:49:14 +0000

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" This script return cities and states """
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT c.name FROM cities c
+                JOIN states s ON c.state_id = s.id
+                WHERE s.name = %s ORDER BY c.id""", (argv[4],))
+    rows = cur.fetchall()
+    cont = 0
+    for row in rows:
+        if cont < len(rows) - 1:
+            print("{}, ".format(row[0]), end="")
+            cont += 1
+        else:
+            print("{}".format(row[0]))
+    cur.close()
+    db.close()

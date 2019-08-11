cf3aa4c1c86267ca05412b47b30726a0b9c62d37
arleybri18 <arleybri18@gmail.com>
2019-08-11 02:43:35 +0000

    Use SQLAlchemy to create a model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique= True, nullable=False, autoincrement=True)
+    name =  Column(String(128), nullable=False)

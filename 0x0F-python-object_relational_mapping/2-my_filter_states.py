5bd044692037be4a8d625deb8d2a9f3bf02ad300
arleybri18 <arleybri18@gmail.com>
2019-08-11 02:29:24 +0000

    implement format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE
-                name= %s ORDER BY id""", (argv[4],))
+    cur.execute("""SELECT * FROM states
+                WHERE name LIKE BINARY '{}'
+                ORDER BY id""".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)

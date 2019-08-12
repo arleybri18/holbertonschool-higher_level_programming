a1c5fa34e594e3487ec8c6804f5e096b916b2990
arleybri18 <arleybri18@gmail.com>
2019-08-12 00:30:57 +0000

    fixed commit position

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).all()
+    states = session.query(State).filter(State.name.like('%a%'))
     for state in states:
         session.delete(state)
-        session.commit()
+    session.commit()
     session.close()

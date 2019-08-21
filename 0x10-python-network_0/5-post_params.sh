b9acac0d99074c9f97ffe03e78d2897163897d39
Yony Brinez <arleybri18@gmail.com>
2019-08-21 03:46:30 +0000

    [Feature] send parameters with POST Method

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send parameters with POST method
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST $1
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -sX POST $1

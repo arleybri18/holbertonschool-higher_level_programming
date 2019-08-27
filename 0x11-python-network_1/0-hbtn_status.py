b727522342053aeb3565bca676abec0033ff9d05
arleybri18 <arleybri18@gmail.com>
2019-08-27 05:14:49 +0000

    [Fix] order for checkers

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ use urlib for make requests
 """
-import urllib.request as req
 
-url = req.Request("https://intranet.hbtn.io/status")
-with req.urlopen(url) as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf-8 content: {}".format(html.decode('utf-8')))
+if __name__ == "__main__":
+    import urllib.request as url
+    req = url.Request("https://intranet.hbtn.io/status")
+    with url.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf-8 content: {}".format(html.decode('utf-8')))

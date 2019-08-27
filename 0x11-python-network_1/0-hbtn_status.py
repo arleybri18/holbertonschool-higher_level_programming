4ba5afe6b12ed9d5ae83db4451d7aff5c3711430
arleybri18 <arleybri18@gmail.com>
2019-08-27 04:35:11 +0000

    [Feature] implement get request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" use urlib for make requests """
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:")
+    print("\t- Type: {}".format(type(html)))
+    print("\t- Content: {}".format(html))
+    print("\t- utf-8 content: {}".format(html.decode('utf-8')))

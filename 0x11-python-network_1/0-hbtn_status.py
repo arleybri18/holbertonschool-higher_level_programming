1f2af63e5499beb18b315607417992977e54ae71
arleybri18 <arleybri18@gmail.com>
2019-08-28 19:41:28 +0000

    [Fix] dellete spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with url.urlopen(req) as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf-8 content: {}".format(html.decode('utf-8')))
+        print("\t- type:{}".format(type(html)))
+        print("\t- content:{}".format(html))
+        print("\t- utf-8 content:{}".format(html.decode('utf-8')))

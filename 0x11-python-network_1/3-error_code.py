7019b399ac5cb61875e83cb7211403558777224b
arleybri18 <arleybri18@gmail.com>
2019-08-28 20:09:07 +0000

    [Feature] capture http errors

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''send request and decode utf-8
+'''
+import urllib.request as url
+import urllib.error as urlerr
+import sys
+
+try:
+    req = url.Request(sys.argv[1])
+    with url.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+except urlerr.HTTPError as e:
+    print("Error code:", e.code)

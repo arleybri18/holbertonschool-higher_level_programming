e6a2a1f4df73b93db7bb15dcaab0474da34e257b
arleybri18 <arleybri18@gmail.com>
2019-08-29 03:09:32 +0000

    [Feature] consume json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import requests
+import sys
+
+try:
+    if len(sys.argv) < 2:
+        s = ""
+    else:
+        s = sys.argv[1]
+    data = {'q': s}
+    url = 'http://0.0.0.0:5000/search_user'
+    res = requests.post(url, data)
+    res = res.json()
+    print(res)
+    if res:
+        print("[{}] {}".format(res.get('id'), res.get('name')))
+    else:
+        print("No result")
+except Exception:
+    print("Not a valid JSON")

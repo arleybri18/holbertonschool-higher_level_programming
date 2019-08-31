08c50214c53cada3c4c19be01075e316fcb62601
arleybri18 <arleybri18@gmail.com>
2019-08-31 19:12:37 +0000

    [Fix] implement break

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,22 +5,19 @@ import requests
 import sys
 
 try:
-    list_commits = []
     owner = sys.argv[1]
     repo = sys.argv[2]
     url = 'https://api.github.com/repos/'+owner+"/"+repo+"/commits"
     res = requests.get(url)
     res = res.json()
+    cont = 1
     for el in res:
         commit = el.get('commit')
         sha = el.get('sha')
         author = commit.get('author').get('name')
-        date = commit.get('author').get('date')
-        tupla = sha, author, date
-        list_commits.append(tupla)
-        tupla = ()
-        list_commits.sort(key=lambda k: k[2])
-    for i in range(10):
-        print(list_commits[i][0] + ': ' + list_commits[i][1])
+        print("{}: {}".format(sha, author))
+        if cont == 10:
+            break
+        cont += 1
 except Exception as e:
     print(e)

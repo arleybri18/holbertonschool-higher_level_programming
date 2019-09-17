75a9c7c5e891077c2f641bf15da2f70011b0b288
arleybri18 <arleybri18@gmail.com>
2019-09-17 18:55:20 +0000

    [fix] semistandar

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-arr.forEach(function(el){
-	console.log(el);
-})
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+arr.forEach(function (el) {
+  console.log(el);
+});

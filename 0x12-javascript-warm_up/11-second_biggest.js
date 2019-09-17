176866e1effb16ffa1c8070851d4ebc5d2a18c13
arleybri18 <arleybri18@gmail.com>
2019-09-17 22:35:16 +0000

    [feature] sorted and find second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const arg = process.argv;
+const len = arg.length;
+const arr = [];
+let i, j;
+if (len > 2) {
+  /* create new array */
+  j = 0;
+  for (i = 2; i < len; i++) {
+    arr[j] = arg[i];
+    j++;
+  }
+  console.log(arr.sort()[j - 2]);
+} else {
+  console.log(0);
+}

3ef244996ddc1aaf36145105bf0c8014ce62a61a
arleybri18 <arleybri18@gmail.com>
2019-09-24 17:46:10 +0000

    [fix] send to standar error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (e, data) {
-  if (e) return console.log(e);
+  if (e) return console.error(e);
   process.stdout.write(data);
 });

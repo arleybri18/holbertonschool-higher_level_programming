e6a7654710b0f865231c9bc688e082f14b1697e0
arleybri18 <arleybri18@gmail.com>
2019-09-24 04:03:38 +0000

    [Feature] read and write files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
-function Concat (a){
-	return fs.readFile(a, 'utf8', function(err, data) {
-		return data;
-	});
-};
-console.log(process.argv[2]);
+function Concat (a) {
+  return (fs.readFileSync(a, 'utf8', function (e, data) {
+    if (e) throw e;
+    return data;
+  }));
+}
 const first = Concat(process.argv[2]);
 const second = Concat(process.argv[3]);
-console.log(first);
-console.log(second);
-fs.writeFile(process.argv[4], ""+first + second+"", function(){});
+fs.appendFile(process.argv[4], first, function (e) {
+  if (e) throw e;
+});
+fs.appendFile(process.argv[4], second, function (e) {
+  if (e) throw e;
+});

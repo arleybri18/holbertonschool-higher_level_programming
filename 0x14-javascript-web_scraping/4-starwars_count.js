de3d4f8696c227efd7447e1ed4e4b5252f1bb1ca
arleybri18 <arleybri18@gmail.com>
2019-09-24 21:24:54 +0000

    [Feature] implement map, reduce, filter and include

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,13 +7,15 @@ request(url, function (error, response, body) {
     console.log(error);
   } else {
     const result = JSON.parse(body).results;
+    /* map return a new list */
     const listChar = result.map(listC => listC.characters);
-    let count = 0;
-    listChar.filter(function (it) {
-      if (it.includes('https://swapi.co/api/people/18/')) {
-        count++;
-      }
-    });
+    /* reduce flat list of lists in a flat list */
+    const li = listChar.reduce((acc, it) => [...acc, ...it]);
+    /* filter return an array with elements that match condition */
+    /* includes validate if the value is present */
+    /* https://www.tutorialrepublic.com/javascript-reference/javascript-array-object.php */
+    /* https://www.freecodecamp.org/news/15-useful-javascript-examples-of-map-reduce-and-filter-74cbbb5e0a1f/ */
+    const count = li.filter(ot => ot.includes('/18/')).length;
     console.log(count);
   }
 });

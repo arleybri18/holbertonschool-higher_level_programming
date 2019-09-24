e0e1ef411440d5e3a9726875d7cd4110588eebb8
arleybri18 <arleybri18@gmail.com>
2019-09-24 00:44:47 +0000

    [Feature] print square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+/* Define class Square */
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  /* constructor call super constructor */
+  constructor (size) {
+    super(size, size);
+  }
+	charPrint (c) {
+		if (c) {
+  /* instance method that print the rectangle */
+			let a = this.height;
+			while (a > 0) {
+				console.log(c.repeat(this.width));
+				a--;
+			}
+		} else {
+			this.print();
+		}
+	}
+}
+module.exports = Square;

835bc6ba52e2aee5c20ae77a1ee1be3d59e5ad28
arleybri18 <arleybri18@gmail.com>
2019-09-24 01:44:04 +0000

    [Feature] reverse array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-	let arr = [];
-	let i = list.length - 1;
-	for (i ; i > 0; i--) {
-		arr.push(list[i]);
-	}
-	return arr;
-}
+  const arr = [];
+  let i = list.length - 1;
+  for (i; i > 0; i--) {
+    arr.push(list[i]);
+  }
+  return arr;
+};

d1cef15439a9db62f84004d0d5f5f949c28f2e4a
arleybri18 <arleybri18@gmail.com>
2019-09-24 21:05:28 +0000

    [Feature] implement filter and map to find a string in url API

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const result = JSON.parse(body).results;
+    const listChar = result.map(listC => listC.characters);
+    let count = 0;
+    listChar.filter(function (it) {
+      if (it.includes('https://swapi.co/api/people/18/')) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+});
